4f1de75b86d624dcb737f4d6703843a888da02f2
pyz <13556920608@qq.com>
2018-11-08 14:05:23 +0800

    aa

--- app.js
+++ app.js
@@ -18,15 +18,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/upload',upload);
 // catch 404 and forward to error handler
 app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'/'));
+    res.sendFile(path.join(__dirname,'/build/index.html'));
 })
 
 app.use(function(req, res, next) {
